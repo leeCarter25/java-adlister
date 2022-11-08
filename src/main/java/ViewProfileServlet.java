6f254c3cdee98500b98bba68b3efdb3037ba2664
Christian Carter <carter.lee2028@gmail.com>
2022-11-07 19:04:38 -0600

    java 3 sessions and cookies exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        // Track session
+        HttpSession session = request.getSession();
+        // set an attribute named username, with the value of ducksOverducks
+        session.setAttribute("username", "ducksOverducks");
+
+        // print the value of the session key username to the console.
+        System.out.println(session.getAttribute("username"));
+
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
